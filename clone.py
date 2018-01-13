7107c42b38b5fe4183fb3231552a97ba8d949a0f
Khoa Ho <hokhoa@grinnell.edu>
2018-01-13 19:53:44 +0700

    Update pipeline

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout, Flatten, Reshape, Lambda
+from keras.layers import Dense, Activation, Dropout, Flatten, Reshape, Lambda, Cropping2D
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
@@ -24,10 +24,13 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 name = os.path.join('IMG', batch_sample[0].split('/')[0])
+                print(name)
                 center_image = cv2.imread(name)
+                center_image_flipped = cv2.flip(center_image, 1)
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                center_angle_flipped = center_angle*(-1)
+                images.extend([center_image, center_image_flipped])
+                angles.extend([center_angle, center_angle_flipped])
             
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -44,12 +47,17 @@ model = Sequential()
 model.add(Lambda(lambda x: x/127.5 - 1.,
                  input_shape=shape,
                  output_shape=shape))
-model.add(Flatten(input_shape=shape))
+model.add(Cropping2D(cropping=((70, 22), (0, 0))))
+model.add(Flatten())
+model.add(Dense(10))
+model.add(Activation('relu'))
 model.add(Dense(1))
-
+# Train the model
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, 
                     steps_per_epoch=np.ceil(len(train_samples)/BATCH_SIZE),
                     validation_data=valid_generator,
                     validation_steps=np.ceil(len(train_samples)/BATCH_SIZE),
                     epochs=3)
+# Save the model
+model.save('model.h5')
